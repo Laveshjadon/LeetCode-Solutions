270aac366cccf2758303b02a5909169ec96a423b
lavesh Jadon <164332184+Laveshjadon@users.noreply.github.com>
2026-05-07 02:19:18 +0530

    Time: 35 ms (77.57%) | Memory: 13.4 MB (75.55%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.py
+++ 402-remove-k-digits/remove-k-digits.py
@@ -1,16 +1,16 @@
 class Solution(object):
     def removeKdigits(self, num, k):
         stack = []
-        for i in range(len(num)):
-            while k > 0 and stack and num[stack[-1]] > num[i]:
+        for digit in num:
+            while k > 0 and stack and stack[-1] > digit:
                 
                 stack.pop()
                 k -= 1
-            stack.append(i)
+            stack.append(digit)
         while k > 0:
             stack.pop()
             k -= 1
-        result = ''.join(num[i] for i in stack).lstrip('0')
-        return result if result else "0"
+        result = ''.join(stack).lstrip('0')
+        return result or '0'
 
         

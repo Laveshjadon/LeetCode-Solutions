2b4a45489122aab089bb8edd65343bd55a3776d9
lavesh Jadon <164332184+Laveshjadon@users.noreply.github.com>
2026-06-18 00:54:48 +0530

    Time: 37 ms (46.22%) | Memory: 12.8 MB (20.55%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -3,13 +3,11 @@ class Solution(object):
     # step 1 we create a hashmap  and add all  magazine
     # step 2 now we compare one by one all ransom value are in hash or not if not getting any of number raise false else true
         hash1 = {} 
-        for i in range(len(magazine)):
-            if magazine[i] not in hash1:
-                hash1[magazine[i]] = 0
-            hash1[magazine[i]] += 1
-        for i in range(len(ransomNote)):
-            if ransomNote[i] not in hash1 or hash1[ransomNote[i]] == 0:
+        for ch in magazine:
+            hash1[ch] = hash1.get(ch,0) + 1
+        
+        for ch in ransomNote:
+            if ch not in hash1 or hash1[ch] == 0:
                 return False
-            
-            hash1[ransomNote[i]] -= 1
+            hash1[ch] -= 1
         return True

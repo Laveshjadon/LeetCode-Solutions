21307d60038ae81de1d679c6bb6695c0a3c4e731
lavesh Jadon <164332184+Laveshjadon@users.noreply.github.com>
2026-06-19 01:46:42 +0530

    Time: 4 ms (49.28%) | Memory: 13.3 MB (16.63%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,13 @@
 class Solution(object):
     def twoSum (self,arr, target):
-        seen ={}
-        for i, num in enumerate(arr):
-            need = target - num
-
-            if need in seen:
-                return [seen[need],i]
-            seen[num] = i
+        # step 1 we add the arr values in the dict and we map them with index
+        # step 3 now we find which number we need if it was in hash than we return both index 
+        # step 3 if not than just map value with number
+        hashh = {}
+        for i in range(len(arr)):
+            need = target - arr[i]
+            if need in hashh:
+                return [hashh[need],i]
+            hashh[arr[i]] = i
+        return []
         

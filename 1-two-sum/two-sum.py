76237598e189c727a9b636f6a065ad8832d206f4
lavesh Jadon <164332184+Laveshjadon@users.noreply.github.com>
2026-06-20 22:34:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (67.55%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,11 @@
 class Solution(object):
-    def twoSum (self,arr, target):
-        # step 1 we add the arr values in the dict and we map them with index
-        # step 3 now we find which number we need if it was in hash than we return both index 
-        # step 3 if not than just map value with number
+    def twoSum(self, nums, target):
         hashh = {}
-        for i in range(len(arr)):
-            need = target - arr[i]
-            if need in hashh:
-                return [hashh[need],i]
-            hashh[arr[i]] = i
-        return []
-        
+        require = 0
+        for i in range(len(nums)):
+            require = target - nums[i]
+            if require in hashh:
+                return [hashh[require],i]
+            hashh[nums[i]] = i 
+        return False
+            

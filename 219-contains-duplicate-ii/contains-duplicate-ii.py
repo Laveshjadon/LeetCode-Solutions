c948e3958f2bc7529374e9b9168dc9d1dfac34f6
lavesh Jadon <164332184+Laveshjadon@users.noreply.github.com>
2026-06-21 02:17:38 +0530

    Time: 35 ms (63.18%) | Memory: 25 MB (33.23%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        hashh = {}
+        for i in range(len(nums)):
+            if nums[i] in hashh and abs(hashh[nums[i]]-i) <= k:
+                return True
+            hashh[nums[i]] = i
+        return False
+            

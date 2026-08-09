5f4464bd0920746cad5a770fa70d9ae7b46bd513
lavesh Jadon <164332184+Laveshjadon@users.noreply.github.com>
2026-08-10 02:56:52 +0530

    Time: 15 ms (9.61%) | Memory: 12.3 MB (92.86%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,41 +1,46 @@
 class Solution(object):
     def isValidSudoku(self, board):
-        # checking rows 
+        # Rows 
         for i in range(9):
             seen = set()
             for j in range(9):
-
+                
                 if board[i][j] == '.':
                     continue
                 if board[i][j] in seen:
                     return False
                 seen.add(board[i][j])
-        
-        # checking columns
+
+        # checking col
         for j in range(9):
             seen = set()
             for i in range(9):
-
+                
                 if board[i][j] == '.':
                     continue
                 if board[i][j] in seen:
                     return False
                 seen.add(board[i][j])
-        # now blocks
-        for rows in range(0,9,3):
-            for columns in range(0,9,3):
 
+
+        # cheking box of 3 * 3
+        for rows in range(0,9,3):
+            for col in range(0,9,3):
                 seen = set()
                 for i in range(rows, rows+3):
-                    for j in range(columns, columns+3):
+                    for j in range(col, col+3):
+                
                         if board[i][j] == '.':
                             continue
                         if board[i][j] in seen:
                             return False
                         seen.add(board[i][j])
 
+
         return True
 
+                
+        
 
 
         

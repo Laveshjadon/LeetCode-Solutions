2fceb8a9eb8c7a6ae4f005cb0a71d41670aeae3d
lavesh Jadon <164332184+Laveshjadon@users.noreply.github.com>
2026-08-12 23:15:59 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (53.02%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def twoSum(self, nums, target):
         hashh = {}
-        require = 0
         for i in range(len(nums)):
-            require = target - nums[i]
-            if require in hashh:
-                return [hashh[require],i]
-            hashh[nums[i]] = i 
-        return False
-            
+            value = target - nums[i]
+            if value in hashh:
+                return [hashh[value],i]
+            else:
+                hashh[nums[i]] = i
+        
+        

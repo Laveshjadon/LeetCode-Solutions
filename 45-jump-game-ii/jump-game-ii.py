3d0f2be738c9c4789c09b4cd21973790fa81e293
lavesh Jadon <164332184+Laveshjadon@users.noreply.github.com>
2026-09-02 15:13:58 +0530

    Time: 6 ms (85.17%) | Memory: 13.2 MB (50.94%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,15 +1,11 @@
 class Solution(object):
     def jump(self, nums):
-        # keep track of maximum 
-        keep_maximum = 0
-        # how many moves require it only moves when are index reach to same value to compare
-        move = 0
-        # it track the value we get by that are we finish the array if not then we took maximum value when index reach to the same value of compare
-        compare = 0 
+        jump = 0
+        furthest = 0
+        one = 0 
         for i in range(len(nums)-1):
-            keep_maximum = max(keep_maximum,nums[i]+i)
-            if i == compare:
-                move += 1
-                compare = keep_maximum
-        return move
-        
+            furthest = max(furthest,i+nums[i])
+            if i == one:
+                jump += 1
+                one = furthest
+        return jump

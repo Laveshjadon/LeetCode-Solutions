82f07c3c33141e6309060f654afd3a4ee0c255f5
lavesh Jadon <164332184+Laveshjadon@users.noreply.github.com>
2026-09-08 12:05:35 +0530

    Time: 2 ms (47.95%) | Memory: 12.7 MB (54.55%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -2,12 +2,13 @@ class Solution(object):
     def generateParenthesis(self, n):
         result = []
         def backtrack(path,start,end):
-            if len(path) == n * 2:
+            if n * 2 == len(path):
                 result.append(path[:])
                 return
             if start < n:
-                backtrack(path + "(",start + 1,end)
+                backtrack(path + '(',start + 1,end)
             if end < start:
-                backtrack(path + ")",start, end + 1)
-        backtrack("",0,0)
+                backtrack(path + ')',start,end + 1)
+        backtrack('',0,0)
         return result
+            

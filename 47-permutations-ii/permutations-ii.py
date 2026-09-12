151e84c5cfff91fd541111bf2d1f32e2d4e014ff
lavesh Jadon <164332184+Laveshjadon@users.noreply.github.com>
2026-09-12 23:12:58 +0530

    Time: 7 ms (77.59%) | Memory: 13.1 MB (18.59%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        result = []
+        nums.sort()
+        def backtrack(path,used):
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+            for i in range(len(nums)):
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+                    continue
+                if used[i]:
+                    continue
+                used[i] = True
+                path.append(nums[i])
+                backtrack(path,used)
+                path.pop()
+                used[i] = False
+        backtrack([],[False]*len(nums))
+        return result
+                
+        

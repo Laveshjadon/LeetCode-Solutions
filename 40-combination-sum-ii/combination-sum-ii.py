54447655c7d689fa4fd840ff838f7d57e5986531
lavesh Jadon <164332184+Laveshjadon@users.noreply.github.com>
2026-09-12 19:33:11 +0530

    Time: 39 ms (30.51%) | Memory: 12.5 MB (8.02%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -2,21 +2,19 @@ class Solution(object):
     def combinationSum2(self, candidates, target):
         result = []
         candidates.sort()
-        def backtrack(path,remaining, index):
-            if remaining == 0:
+        def backtrack(path,i,defference):
+            if defference == 0:
                 result.append(path[:])
                 return
-            if remaining < 0:
-                return 
-            for i in range(index,len(candidates)):
-                if i > index and candidates[i] == candidates[i-1]:
+            if defference < 0:
+                return
+            for j in range(i,len(candidates)):
+                if j >i and candidates[j] == candidates[j-1]:
                     continue
-                if candidates[i] > remaining:
-                    break
-                path.append(candidates[i])
-                backtrack(path,remaining - candidates[i],i+1)
+                path.append(candidates[j])
+                backtrack(path,j+1,defference-candidates[j])
                 path.pop()
-        backtrack([],target,0)
+        backtrack([],0,target)
         return result
 
 
